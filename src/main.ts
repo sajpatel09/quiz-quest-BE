125f9742cf7e803b32986368e4df579226128e33
SAJ VANANI <67995925+sajpatel09@users.noreply.github.com>
2025-07-28 11:21:15 +0530

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -6,21 +6,18 @@ import cookieParser from 'cookie-parser';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  const allowedOrigins = (process.env.ALLOWED_ORIGINS || '').split(',');
-  app.enableCors({
-    origin: (
-        origin: string,
-        callback: (err: Error | null, allow?: boolean) => void,
-    ) => {
-      if (!origin || allowedOrigins.indexOf(origin) !== -1) {
-        callback(null, true);
-      } else {
-        callback(new Error('Not allowed by CORS'));
-      }
-    },
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
-    credentials: true,
-  });
+  // --- This is the simplified and standard CORS block ---
+  const allowedOrigins = process.env.ALLOWED_ORIGINS?.split(',');
+
+  if (allowedOrigins) {
+    app.enableCors({
+      origin: allowedOrigins,
+      methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+      credentials: true,
+    });
+  }
+  // ----------------------------------------------------
+
   app.use(cookieParser());
 
   app.useGlobalPipes(
